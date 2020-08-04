a01623879a1b6c6c3171216e3f8705c6ba72c291
jahanzaibshahid <jahanzaibshahid07@gmail.com>
2020-08-04 18:40:45 +0500

    first commit

---
+++ Controllers/PaymentDetailsController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailsController : ControllerBase
+    {
+        private readonly PaymentDetailContext _context;
+
+        public PaymentDetailsController(PaymentDetailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentDetailTB>>> GetPaymentDetail()
+        {
+            return await _context.PaymentDetail.ToListAsync();
+        }
+
+
+        // PUT: api/PaymentDetails/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentDetail(int id, PaymentDetailTB paymentDetail)
+        {
+            if (id != paymentDetail.PmId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paymentDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PaymentDetails
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetailTB>> PostPaymentDetail(PaymentDetailTB paymentDetail)
+        {
+            _context.PaymentDetail.Add(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentDetail", new { id = paymentDetail.PmId }, paymentDetail);
+        }
+
+        // DELETE: api/PaymentDetails/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<PaymentDetailTB>> DeletePaymentDetail(int id)
+        {
+            var paymentDetail = await _context.PaymentDetail.FindAsync(id);
+            if (paymentDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.PaymentDetail.Remove(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return paymentDetail;
+        }
+
+        private bool PaymentDetailExists(int id)
+        {
+            return _context.PaymentDetail.Any(e => e.PmId == id);
+        }
+    }
+}
